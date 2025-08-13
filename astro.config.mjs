3cdb6d6ce97714903ff5cad370a8d672304ef8e7
Max <seunghyun5767@gmail.com>
2025-08-13 21:14:33 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://Sado.github.io/",
+	site: "https://sado.github.io/",
 	base: "/",
 	output: 'static',
 	trailingSlash: "always",
