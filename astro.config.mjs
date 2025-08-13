ddc4280fd9c570f934acdb61c1b3d223a5a56430
Max <seunghyun5767@gmail.com>
2025-08-13 20:21:05 +0900

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,9 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://Sado.github.io/",
 	base: "/",
+	output: 'static',
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
