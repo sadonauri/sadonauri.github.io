39cd653afb82edaf4ec0352b6fe2b5bf2af90a2e
Max <seunghyun5767@gmail.com>
2025-08-13 22:15:55 +0900

    little

--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/ssung5767", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/sadonauri", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
@@ -72,7 +72,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/ssung5767",
+			url: "https://github.com/sadonauri",
 		},
 	],
 };
